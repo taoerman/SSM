7e4c35e875f15bbe647b9ca97641cd6c371fc4d1
taoerman <qiliu0523@gmail.com>
2025-01-09 20:19:36 -0800

    create a component person and then injecting into ioc

---
+++ ssm-parent/spring-01-ioc/src/main/java/com/example/spring01ioc/Bean/Person.java
@@ -0,0 +1,21 @@
+package com.example.spring01ioc.Bean;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+}
+/*
+@Data is a Lombok annotation that generates all the boilerplate code typically associated with simple “data-holding” classes.
+In particular, it does the following automatically:
+	•	@Getter and @Setter for all fields (generates getters and setters)
+	•	@ToString (a toString method including all fields)
+	•	@EqualsAndHashCode (equals and hashCode methods based on the fields)
+	•	@RequiredArgsConstructor for final fields (generates a constructor if any fields are final or marked with @NonNull)
+
+Essentially, @Data bundles together these Lombok annotations
+so you don’t have to write them individually, reducing boilerplate.
+ */
